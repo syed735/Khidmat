e1f9a3fe5be03be86054739d7028b374cd1aa414
syed735 <syedsufiyan964@gmail.com>
2024-12-12 12:15:57 +0530

    Update database configuration and dependencies for PostgreSQL

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -2,24 +2,23 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager, login_manager
+from flask_login import LoginManager
 
+login_manager = LoginManager()
+app = Flask(__name__)
 
+# Set secret key
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'mysecretkey')
 
+# Database configuration
+DATABASE_URL = "postgresql://khidmat_db_user:hJdPNpbLOHz6hX9ih8vZnDMOm7stkIXW@dpg-ctd89o1u0jms73f46vd0-a/khidmat_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize database
+db = SQLAlchemy(app)
+Migrate(app, db)
 
-login_manager= LoginManager()
-
-app=Flask(__name__)
-
-app.config['SECRET_KEY']='mysecretkey'
-basedir=os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+ os.path.join(basedir,'data.sqlite')
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-db= SQLAlchemy(app)
-Migrate(app,db)
-
+# Setup login manager
 login_manager.init_app(app)
-
-login_manager.login_view="login"
+login_manager.login_view = "login"
